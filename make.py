4ca75ca3e3c9ed4708b902046c67ff9884c2630a
Michael McCormick <michael@thousandleaves.org>
2017-02-05 11:38:25 -0600

    simple app, infra stack, codebuild

---
+++ make.py
@@ -0,0 +1,15 @@
+import os
+from string import Template
+
+
+substitutions = dict(
+    WELCOME="Welcome!",
+    MESSAGE="Wow, stuff worked!",
+    BUILD=os.environ.get("BUILD", "SNAPSHOT")
+    )
+
+with open("app/index.html") as f:
+    template = Template(f.read())
+
+with open("build/index.html", "w") as f:
+    f.write(template.substitute(substitutions))
