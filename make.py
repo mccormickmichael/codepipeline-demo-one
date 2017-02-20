f0e0d9018371dc313602da3de3e9bc34a41a92cb
Michael McCormick <michael@thousandleaves.org>
2017-02-19 18:19:44 -0600

    add another variable to the app

--- make.py
+++ make.py
@@ -5,7 +5,8 @@ from string import Template
 substitutions = dict(
     WELCOME="Welcome!",
     MESSAGE="Wow, stuff worked!",
-    BUILD=os.environ.get("BUILD", "SNAPSHOT")
+    BUILD=os.environ.get("BUILD", "SNAPSHOT"),
+    THING=os.environ.get("THING", "NOTHING")
     )
 
 with open("app/index.html") as f:
